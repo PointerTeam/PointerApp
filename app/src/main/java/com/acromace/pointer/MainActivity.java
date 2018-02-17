6a9ba62162007795ef32842125fdb5ed5eff220a
Andy Cho <hello@andycho.ca>
2018-02-17 14:30:08 -0500

    Move to CreatePoint tab when tapping FAB in Main
    This will be a screen used to create new points and send it to the
    server. Right now there's nothing there. Also there's no button to go
    back on that screen so you have to press the back button.
    
    It's mostly there right now as an example of how to move to a new
    Activity.

--- app/src/main/java/com/acromace/pointer/MainActivity.java
+++ app/src/main/java/com/acromace/pointer/MainActivity.java
@@ -1,20 +1,13 @@
 package com.acromace.pointer;
 
-import android.os.AsyncTask;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity implements PingCallbackInterface {
 
@@ -23,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements PingCallbackInter
     private Server server = new Server();
     private Button pingButton;
     private TextView pingResponseTextView;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +25,21 @@ public class MainActivity extends AppCompatActivity implements PingCallbackInter
 
         pingButton = findViewById(R.id.ping_button);
         pingResponseTextView = findViewById(R.id.ping_response_textview);
+        fab = findViewById(R.id.add_fab);
 
+        setupFab();
         setupPingButton();
+    }
+
+    private void setupFab() {
+        final MainActivity mainActivity = this;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mainActivity, CreatePointActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void setupPingButton() {
