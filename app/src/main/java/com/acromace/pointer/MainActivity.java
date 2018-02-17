426602ae40b4d840356101f2359c07947d05778b
Andy Cho <hello@andycho.ca>
2018-02-17 12:10:42 -0500

    Add a server class and use PythonAnywhere endpoint

--- app/src/main/java/com/acromace/pointer/MainActivity.java
+++ app/src/main/java/com/acromace/pointer/MainActivity.java
@@ -16,13 +16,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PingCallbackInterface {
 
     private static final String TAG = "MainActivity";
 
+    private Server server = new Server();
     private Button pingButton;
     private TextView pingResponseTextView;
-    private String pingOutputText; // This is terrible, will refactor
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,44 +50,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void makePingRequest() {
-        Log.v(TAG, "Ping!");
         pingResponseTextView.setText("Making request to server...");
-        final MainActivity mainActivity = this;
-        String outputText;
+        server.makePingRequest(this);
+    }
 
-        AsyncTask.execute(new Runnable() {
+    @Override
+    public void pingResponse(final String response) {
+        runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                try {
-                    // This magic URL is used to connect to "localhost" on your computer
-                    // instead of the "localhost" on the emulator
-                    final URL pingEndpoint = new URL("http://10.0.2.2:5000/ping");
-                    final HttpURLConnection myConnection =
-                            (HttpURLConnection) pingEndpoint.openConnection();
-                    final int responseCode =myConnection.getResponseCode();
-                    if (responseCode != 200) {
-                        Log.w(TAG, String.format("Server returned status code: %d", responseCode));
-                        pingOutputText = String.format("Server returned status code: %d", responseCode);
-                    }
-
-                    final InputStream inputStream = myConnection.getInputStream();
-                    final Scanner scanner = new Scanner(inputStream, "UTF-8");
-                    final String response = scanner.next();
-                    Log.d(TAG, response);
-                    pingOutputText = response;
-                } catch (MalformedURLException e) {
-                    Log.e(TAG, "URL provided was malformed");
-                    pingOutputText = e.getLocalizedMessage();
-                } catch (java.io.IOException e) {
-                    Log.e(TAG, "Error while opening connection to the server");
-                    pingOutputText = e.getLocalizedMessage();
-                }
-                mainActivity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        pingResponseTextView.setText(pingOutputText);
-                    }
-                });
+                pingResponseTextView.setText(response);
             }
         });
     }
---
+++ app/src/main/java/com/acromace/pointer/Server.java
@@ -0,0 +1,62 @@
+package com.acromace.pointer;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by acromace on 2018-02-17.
+ */
+
+interface PingCallbackInterface {
+    void pingResponse(final String response);
+}
+
+public class Server {
+
+    private static final String TAG = "Server";
+    private static final String SERVER_LOCALHOST = "http://10.0.2.2:5000/";
+    private static final String SERVER_REMOTE = "http://acromace.pythonanywhere.com/";
+
+    private static final String SERVER = SERVER_REMOTE; // Change this to SERVER_LOCALHOST to test locally
+
+    public void makePingRequest(final PingCallbackInterface callback) {
+        Log.v(TAG, "Ping!");
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+            String pingOutputText;
+            try {
+                // This magic URL is used to connect to "localhost" on your computer
+                // instead of the "localhost" on the emulator
+                final URL pingEndpoint = new URL(SERVER + "ping");
+                final HttpURLConnection myConnection =
+                        (HttpURLConnection) pingEndpoint.openConnection();
+                final int responseCode =myConnection.getResponseCode();
+                if (responseCode != 200) {
+                    Log.w(TAG, String.format("Server returned status code: %d", responseCode));
+                }
+
+                final InputStream inputStream = myConnection.getInputStream();
+                final Scanner scanner = new Scanner(inputStream, "UTF-8");
+                final String response = scanner.next();
+                Log.d(TAG, response);
+                pingOutputText = response;
+            } catch (MalformedURLException e) {
+                Log.e(TAG, "URL provided was malformed");
+                pingOutputText = e.getLocalizedMessage();
+            } catch (java.io.IOException e) {
+                Log.e(TAG, "Error while opening connection to the server");
+                pingOutputText = e.getLocalizedMessage();
+            }
+            callback.pingResponse(pingOutputText);
+            }
+        });
+    }
+}
