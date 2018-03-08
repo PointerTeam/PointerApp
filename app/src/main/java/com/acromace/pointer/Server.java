1ce019b333c649ac93e6ea257d3937b0269cac15
Umang080799 <umang.gupta@edu.uwaterloo.ca>
2018-03-07 22:08:06 -0500

    Implemented createPoints

--- app/src/main/java/com/acromace/pointer/Server.java
+++ app/src/main/java/com/acromace/pointer/Server.java
@@ -3,7 +3,14 @@ package com.acromace.pointer;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -19,6 +26,7 @@ interface PingCallbackInterface {
 }
 
 interface CreatePointCallbackInterface {
+
     // Success is true if the point was successfully created, false if not
     // On error, sets errorMessage
     void createPointResponse(final boolean success, final String errorMessage);
@@ -37,14 +45,77 @@ public class Server {
     private static final String SERVER_LOCALHOST = "http://10.0.2.2:5000/";
     private static final String SERVER_REMOTE = "http://acromace.pythonanywhere.com/";
 
-    private static final String SERVER = SERVER_REMOTE; // Change this to SERVER_LOCALHOST to test locally
+    private static final String SERVER = SERVER_LOCALHOST; // Change this to SERVER_LOCALHOST to test locally
 
     void createPoint(final Point point, final CreatePointCallbackInterface callback) {
         // TODO: Implement making the network call
-        Log.d(TAG, "Creating point: " + point.toString());
-        Log.d(TAG, "Creating points is not implemented, returning success");
-        callback.createPointResponse(true, null);
-        //callback.createPointResponse(false, "Error: failed to send message");
+        Log.v(TAG, "Ping!");
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                String pingOutputText;
+                try {
+                    // This magic URL is used to connect to "localhost" on your computer
+                    // instead of the "localhost" on the emulator
+                    final URL createEndpoint = new URL(SERVER + "messages");
+                    final HttpURLConnection myConnection =
+                            (HttpURLConnection) createEndpoint.openConnection();
+                    myConnection.setDoOutput(true);
+                    myConnection.setRequestProperty("Content-Type", "application/json");
+                    myConnection.setRequestProperty("Accept", "application/json");
+                    myConnection.setRequestMethod("POST");
+                    myConnection.connect();
+
+                    LatLng position = point.getPosition();
+                    byte[] outputBytes = ("{\"lat\": "+ position.latitude +", \"lon\": "+ position.longitude+"  , \"message\": " + point.getMessage() + "}").getBytes("UTF-8");
+                    OutputStream os = myConnection.getOutputStream();
+                    os.write(outputBytes);
+                    os.close();
+
+                    final int responseCode =myConnection.getResponseCode();
+                                    switch(responseCode) {
+                        case 200: //all ok
+                            break;
+                        case 401:
+                        case 403:
+                            // authorized
+                            break;
+                        default:
+                            //whatever else...
+                            String httpResponse = myConnection.getResponseMessage();
+                            BufferedReader br = new BufferedReader(new InputStreamReader(myConnection.getErrorStream()));
+                            Log.w(TAG, String.format("Server returned status code: %d", responseCode));
+                            String line;
+                            try {
+                                while ((line = br.readLine()) != null) {
+                                    Log.d("error", "    " + line);
+                                }
+                            } catch (Exception ex) {
+                                //nothing to do here
+                            }
+
+                            break;
+                    }
+                    myConnection.disconnect();
+
+                    final InputStream inputStream = myConnection.getInputStream();
+                    final Scanner scanner = new Scanner(inputStream, "UTF-8");
+                    final String response = scanner.next();
+                    Log.d(TAG, response);
+                    pingOutputText = response;
+                } catch (MalformedURLException e) {
+
+
+                    Log.e(TAG, "URL provided was malformed");
+                    pingOutputText = e.getLocalizedMessage();
+                } catch (java.io.IOException e) {
+                    Log.e(TAG, "Error while opening connection to the server");
+                    pingOutputText = e.getLocalizedMessage();
+                }
+                //callback.pingResponse(pingOutputText);
+            }
+        });
     }
 
     void getPoints(final double latitude, final double longitude, final GetPointsCallbackInterface callback) {
