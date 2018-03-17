ee29a60f9af5614b63979c27eeda341df1b4bd71
Luana <lc03711@gmail.com>
2018-03-17 16:29:04 -0400

    modified popup to zoom to marker & offset

--- app/src/main/java/com/acromace/pointer/MainActivity.java
+++ app/src/main/java/com/acromace/pointer/MainActivity.java
@@ -276,6 +276,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
                         popupMessage.setText(marker.getTitle().toString());
 
+                        LatLng pointLoc = marker.getPosition();
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(pointLoc, 15.0f));
+
                         /*
                             public PopupWindow (View contentView, int width, int height)
                                 Create a new non focusable popup window which can display the contentView.
@@ -332,7 +335,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                                 y : the popup's y location offset
                         */
                         // Finally, show the popup window at the center location of root relative layout
-                        popUp.showAtLocation(findViewById(R.id.map), Gravity.CENTER,0,0);
+                        popUp.showAtLocation(findViewById(R.id.map), Gravity.CENTER,0,-200);
 
                         return true;
                     }
