540619ee30ba5baf7469973bfc081c62200be2d0
tkfi <nick.yang3721@gmail.com>
2018-03-24 14:38:17 -0400

    Added Support for Images
    Added to the server so it can take in bitmap images, and then convert them to a string to be stored at locations.

--- app/src/main/java/com/acromace/pointer/Point.java
+++ app/src/main/java/com/acromace/pointer/Point.java
@@ -1,10 +1,17 @@
 package com.acromace.pointer;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.util.Base64;
+
 import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.clustering.ClusterItem;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * Created by acromace on 2018-02-17.
@@ -14,6 +21,7 @@ public class Point implements ClusterItem {
 
     final private LatLng position;
     final private String message;
+    private Bitmap image = null;
 
     Point(final double latitude, final double longitude, final String message) {
         this.position = new LatLng(latitude, longitude);
@@ -23,6 +31,14 @@ public class Point implements ClusterItem {
     Point(final LatLng position, final String message) {
         this.position = position;
         this.message = message;
+    }
+
+    Bitmap getImage() {
+        return image;
+    }
+
+    void setImage(Bitmap b) {
+        image = b;
     }
 
     String getMessage() {
@@ -38,6 +54,16 @@ public class Point implements ClusterItem {
         json.put("lat", position.latitude);
         json.put("lon", position.longitude);
         json.put("message", message);
+        if (image != null) {
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            image.compress(Bitmap.CompressFormat.JPEG, 100, baos); //bm is the bitmap object
+            byte[] b = baos.toByteArray();
+
+            String encodedImage = Base64.encodeToString(b, Base64.DEFAULT);
+
+            json.put("image",encodedImage);
+        }
         return json;
     }
     @Override
